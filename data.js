20878765a5513f68c29e659862d052913a75740c
Amandine Le <thanhtruc.uni@gmail.com>
2026-02-19 22:54:34 +0000

    Add files via upload

---
+++ data.js
@@ -0,0 +1,20 @@
+let allmusic = [
+    {
+        name: "Rhapsody on a Theme of Paganini, Op. 43 - Var 18",
+        artist: "Sergei Rachmaninoff",
+        img: "Music player/image1.jpg",
+        src: "Music/music1.mp3"
+    },
+    {
+        name: "Liszt - Liebestraum No.3 - Love Dream",
+        artist: "Guiomar Novaes",
+        img: "Music player/image2.jpg",
+        src: "Music/music2.mp3"
+    },
+    {
+        name: "Chopin - Fantaisie-Impromptu, Op. 66",
+        artist: "Oriano de Almeida",
+        img: "Music player/image3.jpg",
+        src: "Music/music3.mp3"
+    }
+]
---
+++ script.js
@@ -0,0 +1,39 @@
+// let musicName = document.querySelector(".container .music-details .music-name");
+// let artistName = document.querySelector(".container .music-details .artist-name");
+// let img = document.querySelector(".container .music-img-box .music-img img");
+// let music = document.querySelector(".container #music");
+// let playPauseBtn = document.querySelector(".container .btns-box .btns .play-pause-btn");
+
+// let musicIndex = 2;
+
+// window.addEventListener("load", ()=>{
+//     loadMusic();
+// })
+
+// let loadMusic =()=>{
+//     musicName.innerHTML = `${allmusic[musicIndex - 1].name}`;
+//     artistName.innerHTML = `${allmusic[musicIndex - 1].artist}`;
+//     img.src = `${allmusic[musicIndex - 1].img}.jpg`;
+//     music.src = `${allmusic[musicIndex - 1].src}.mp3`;
+// }
+
+// playPauseBtn.addEventListener("click", ()=>{
+//     if(playPauseBtn.classList.contains("play")){
+//         playPauseBtn.classList.replace("play", "paused");
+//         playPauseBtn.querySelector(".material-icons").innerHTML = "pause";
+//         music.play();
+//     }else{
+//         playPauseBtn.classList.replace("paused", "play");
+//         playPauseBtn.querySelector(".material-icons").innerHTML = "play_arrow";
+//         music.pause();
+
+//     }
+// })
+
+const menuIcon = document.querySelector("#menu-icon");
+const navLinks = document.querySelector(".nav-links");
+
+menuIcon.onclick = () => {
+    navLinks.classList.toggle("active");
+    
+}
