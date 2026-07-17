c5596c79f6a72ddb209e9ccb2b1d158de802a3f5
Nguyễn Nguyên Khương <92970893+nguyennguyenkhuong1212@users.noreply.github.com>
2026-07-18 01:30:01 +0700

    Adjust spacing of About Section

--- script.js
+++ script.js
@@ -30,10 +30,9 @@
 //     }
 // })
 
-const menuIcon = document.querySelector("#menu-icon");
-const navLinks = document.querySelector(".nav-links");
+const menuIcon = document.querySelector('#menu-icon');
+const navLinks = document.querySelector('.nav-links');
 
 menuIcon.onclick = () => {
-    navLinks.classList.toggle("active");
-    
-}
+    navLinks.classList.toggle('active');
+};
